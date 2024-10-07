8c7c4b0d69b549962bc1dce322c83bd3f5e42140
JoshuaRosado <joshua06rosado@gmail.com>
2024-10-07 16:28:23 -0500

    Initial Commit

---
+++ iEdutainment/iEdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  iEdutainmentApp.swift
+//  iEdutainment
+//
+//  Created by Joshua Rosado Olivencia on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct iEdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iEdutainmentTests/iEdutainmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  iEdutainmentTests.swift
+//  iEdutainmentTests
+//
+//  Created by Joshua Rosado Olivencia on 10/7/24.
+//
+
+import Testing
+@testable import iEdutainment
+
+struct iEdutainmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
