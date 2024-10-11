9dcc3cc1685775d39c19e2ad0bf81d7fdac7cac6
JoshuaRosado <joshua06rosado@gmail.com>
2024-10-11 14:20:38 -0500

    Clouds.swift file added

--- iEdutainment/ContentView.swift
+++ iEdutainment/ContentView.swift
@@ -8,61 +8,6 @@
 import SwiftUI
 
 
-struct Clouds: View {
-    var lowerCloud: Double
-    var upperCloud: Double
-    
-    var body: some View{
-        VStack{
-            
-            
-            Section{
-                Circle()
-                    .frame(width: 120, height: 200)
-                    .position(x: 50, y: 400)
-                Circle()
-                    .frame(width: 150, height: 125)
-                    .position(x: 160, y: 150)
-                Circle()
-                    .frame(width: 150, height: 175)
-                    .position(x: 120, y: 0)
-                Circle()
-                    .frame(width: 125, height: 180)
-                    .position(x: 200, y: -200)
-            }
-            .foregroundStyle(.white)
-            .position(x: CGFloat(lowerCloud), y: CGFloat(-50))
-            .animation(.linear(duration: 25).repeatForever(autoreverses: false), value: lowerCloud)
-            .shadow(color: .black.opacity(0.2), radius: 30, x: 100, y: 600)
-        }
-        VStack{
-            
-            
-            Section{
-                Circle()
-                    .frame(width: 120, height: 200)
-                    .position(x: 50, y: 400)
-                Circle()
-                    .frame(width: 150, height: 125)
-                    .position(x: 80, y: 160)
-                Circle()
-                    .frame(width: 190, height: 140)
-                    .position(x: 120, y: 0)
-                Circle()
-                    .frame(width: 150, height: 130)
-                    .position(x: 180, y: -200)
-            }
-        }
-        .foregroundStyle(.white)
-        .position(x: CGFloat(upperCloud), y: CGFloat(120))
-        .animation(.linear(duration: 45).repeatForever(autoreverses: false), value: upperCloud)
-        .shadow(color: .black.opacity(0.2), radius: 30, x: 100, y: 600)
-    }
-}
-
-
-
-
 struct ContentView: View {
     @State private var difficultyRange = ["Easy", "Medium", "Hard"]
     @State private var amountOfQuestions = 0
@@ -99,13 +44,12 @@ struct ContentView: View {
                 Text(play ? "Started": "Start")
                     
             }
-            .buttonStyle(.borderedProminent)
-            .tint(.white.opacity(0.5))
+            
             .shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
             .shadow(color: Color.white.opacity(0.7), radius: 10, x: -5, y: -5)
             .buttonBorderShape(.roundedRectangle)
             .controlSize(.large)
-            .foregroundStyle(.secondary)
+            .foregroundStyle(.white)
             .font(.system(size: 30, weight: .semibold, design: .default))
         }
         
