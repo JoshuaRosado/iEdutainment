8940eaa074e8aafcf5c34ef72995505769f6b7c3
JoshuaRosado <joshua06rosado@gmail.com>
2024-10-18 15:24:12 -0500

    bg mountains struct view added

--- iEdutainment/ContentView.swift
+++ iEdutainment/ContentView.swift
@@ -58,41 +58,7 @@ struct ContentView: View {
                 .zIndex(3)
                 .position(x: 200, y: 375)
                 
-                HStack{
-                    
-                    Circle()
-                        .frame(minWidth: 250)
-                        .foregroundStyle(.green).brightness(0.06)
-                        .zIndex(1)
-                        .position(x: 300, y: 400)
-                        
-                    
-                    Circle()
-                        .frame(minWidth:400)
-                        .foregroundStyle(.green).brightness(0.0)
-                        .zIndex(0)
-                        .position(x: 300, y: 350)
-                    
-                        
-                    Circle()
-                        .frame(minWidth:400)
-                        .foregroundStyle(.green).brightness(0.02)
-                        .position(x: 10 , y: 400)
-
-                    
-                    Circle()
-                        .frame(minWidth:350)
-                        .foregroundStyle(.green).brightness(0.04)
-                        .zIndex(-1)
-                        .position(x: -200, y: 350)
-                        
-
-                }
-    
-                Color.green.brightness(0.09)
-                    .frame(height: 100)
-                    .padding(.bottom, -50)
-                
+                Mountains()
                     
                     
                 
