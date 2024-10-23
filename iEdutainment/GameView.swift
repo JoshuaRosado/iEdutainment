e086ce05e57109be52cc82bbb1ea1c22fb541d07
JoshuaRosado <joshua06rosado@gmail.com>
2024-10-22 20:26:43 -0500

    header, input field and question added

--- iEdutainment/GameView.swift
+++ iEdutainment/GameView.swift
@@ -12,7 +12,9 @@ struct GameView: View {
     @State private var positionLowerCloud: Double = -150
     @State private var positionUpperCloud: Double = -175
     @State private var usersAnswer = ""
-    @State private var multNumbers = Int.random(in: 0...5)
+    @State private var multNumbersEasy = Int.random(in: 0...5)
+    @State private var multNumbersMedium = Int.random(in: 5...10)
+    @State private var multNumbersHard = Int.random(in: 10...20)
     
     var body: some View {
         ZStack{
@@ -20,15 +22,46 @@ struct GameView: View {
             Color.green.brightness(0.4)
                 .ignoresSafeArea()
             
-            HStack{
-                Image("hippo", bundle: .main)
-                    .resizable()
-                    .frame(width: 110, height: 100)
+            VStack{
+                Spacer()
+                HStack{
+                    
+                    Image("hippo", bundle: .main)
+                        .resizable()
+                        .frame(width: 110, height: 100)
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20)
+                            .frame(width: 200, height: 75)
+                            .foregroundStyle(.white)
+                        Text("What is").font(.system(size: 18))
+                    }
+                    
+                    
+                }
+                Spacer()
+                Spacer()
                 ZStack{
+                    
+                    
                     RoundedRectangle(cornerRadius: 20)
                         .frame(width: 200, height: 75)
-                        .foregroundStyle(.white)
-                    Text(" 5 x 2").font(.system(size: 32)).bold()
+                        .foregroundStyle(.secondary)
+                    
+                    Text(" 5 x \(multNumbersEasy)").font(.system(size: 32)).bold().foregroundStyle(.white)
+                }
+                Spacer()
+                
+                ZStack{
+                    RoundedRectangle(cornerRadius: 20).foregroundStyle(.white)
+                        .frame(height: 500)
+                        .padding()
+                    TextField("Hello", text: $usersAnswer)
+                        .titleStyle()
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                        .frame(width: 150, height: 100)
+                        .keyboardType(.numberPad)
+                        
                 }
                 
             }
