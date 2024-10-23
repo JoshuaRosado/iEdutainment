c019189c7524110cb027eedb11b195421d6ee0c0
JoshuaRosado <joshua06rosado@gmail.com>
2024-10-23 16:18:42 -0500

    Questions struct created

---
+++ iEdutainment/QuestionsView.swift
@@ -0,0 +1,16 @@
+//
+//  QuestionsView.swift
+//  iEdutainment
+//
+//  Created by Joshua Rosado Olivencia on 10/23/24.
+//
+
+
+
+import SwiftUI
+
+struct QuestionsView: View {
+    var body: some View {
+        Text("Questions")
+    }
+}
