d899f324eab0c0063ed9acc893186cc3db6257df
JoshuaRosado <joshua06rosado@gmail.com>
2024-10-23 16:22:37 -0500

    score text added

--- iEdutainment/GameView.swift
+++ iEdutainment/GameView.swift
@@ -63,6 +63,7 @@ struct GameView: View {
                         .keyboardType(.numberPad)
                         
                 }
+                Text("Score: 9").titleStyle()
                 
             }
             
